72db3bd3e9b789222856d43c243e8e10fd5d748d
nathanlebowitz <nathanlebowitz212@gmail.com>
2024-06-30 11:42:17 -0400

    changes added

--- index.js
+++ index.js
@@ -1,46 +1,88 @@
 import express from 'express'
 import cors from 'cors'
+import pgPromise from 'pg-promise'
+
+const pgp = pgPromise()
+const db = pgp({
+    host: 'ep-blue-boat-a6g47ocs.us-west-2.retooldb.com',
+    port: '5432',
+    database: 'retool',
+    user: 'retool',
+    password:'t9gNolXYaG7L',
+    ssl: true
+
+})
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use((req, res, next) => {
-    console.log('hello from my middlewear')
-    next()
+
+
+
+
+app.post('/tasks', async (req, res) => {
+    const result = await db.one('insert into todos.task (user_id, title) select id, ${title} from todos.person where name = ${userName} and pass = ${password} returning *', {
+        title: req.body.title,
+        userName: req.body.userName,
+        password: req.body.password
+        
+    } )
+    res.json({
+        title:result.title,
+        done: false,
+        id: result.id,
+        ok: true
+    })
+
 })
 
-function rootHandler(req, res) {
-    res.send('hello world')
-}
+app.get('/tasks/:userName/:password', async (req, res) => {
+    const tasks = await db.manyOrNone('select * from todos.task where deleted_at is null and user_id = (SELECT id  FROM todos.person  WHERE name = ${userName} and pass = ${password})',{
+        userName: req.params.userName,
+        password: req.params.password
 
-app.get('/api', rootHandler)
+    })
+    res.json(tasks.map(task=> ({id: task.id, title: task.title, done: task.status !== 'active'})))
+})
+       
 
-app.post('/tasks', (req, res) => {
-    res.json(req.body)
-
+app.patch('/tasks/:id/:userName/:password', async (req, res) => {
+    const result = await db.none("update todos.task set status = 'done' where id = ${id} and user_id = (SELECT id  FROM todos.person  WHERE name = ${userName} and pass = ${password})",{
+        id:req.params.id,
+        userName:req.params.userName,
+        password: req.params.password
+    })
+    res.json({ok : true})
 })
 
-app.get('/tasks', (req, res) => {
-    const tasks =
-        [{
-            title: ' register on VP',
-            done: true,
-            userName: '',
-            deleted: false
-        }, {
-            title: 'Learn React.JS',
-            done: false,
-            userName: '',
-            deleted: false
-        }, {
-            title: 'Learn Express.JS',
-            done: false,
-            userName: '',
-            deleted: false
-        }]
-    res.json(tasks)
+app.delete('/tasks/:id/:userName/:password' , async (req, res) => {
+    await db.none("update todos.task set deleted_at = now() where id = ${id} and user_id = (SELECT id  FROM todos.person  WHERE name = ${userName} and pass = ${password})",{
+        id: req.params.id,
+        userName:req.params.userName,
+        password: req.params.password
+    })
+    res.json({ok: true})
 })
+
+app.post('/users', async(req, res) =>{
+    const result = await db.one('insert into todos.person (name, pass) values(${name},${password}) returning *',{
+        name: req.body.name,
+        password: req.body.password
+    })
+    res.json({ok:true})
+})
+
+app.post('/login', async(req,res) =>{
+    try{const result = await db.one('select * from todos.person where name = ${name} and pass = ${password}',{
+        name: req.body.name,
+        password: req.body.password
+    })
+    res.json({ok:true})}
+    catch(e)
+    {res.json({ok:false})}
+})
+
 
 
 app.listen('3000', () => {
